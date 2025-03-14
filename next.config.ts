3487775fab5e70c0508c9d94464a6489cdc367b3
Chamithu Ruberu <109074596+ChamithuRuberu@users.noreply.github.com>
2025-03-14 09:51:50 +0530

    package added

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,23 @@
+// next.config.ts
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
   eslint: {
     ignoreDuringBuilds: true,
   },
   typescript: {
     ignoreBuildErrors: true,
   },
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'http://localhost:8080/api/:path*'
+      }
+    ];
+  }
 };
 
 export default nextConfig;

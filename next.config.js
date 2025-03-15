330b8be8726fda7a862f864077f5cf5d6e6e1efe
Chamithu Ruberu <109074596+ChamithuRuberu@users.noreply.github.com>
2025-03-15 07:52:54 +0530

    conflicts push

---
+++ next.config.js
@@ -0,0 +1,21 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'http://localhost:8080/api/:path*'
+      }
+    ];
+  }
+};
+
+module.exports = nextConfig; 

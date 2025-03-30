1080026dbd5bdd1f8885c6c15a073bb008d6f182
Chamithu Ruberu <109074596+ChamithuRuberu@users.noreply.github.com>
2025-03-30 12:25:17 +0530

    dd

--- next.config.js
+++ next.config.js
@@ -9,12 +9,15 @@ const nextConfig = {
     ignoreBuildErrors: true,
   },
   async rewrites() {
-    return [
-      {
-        source: '/api/:path*',
-        destination: 'http://localhost:8080/api/:path*'
-      }
-    ];
+    if (process.env.NODE_ENV === 'development') {
+      return [
+        {
+          source: '/api/:path*',
+          destination: 'http://localhost:8080/api/:path*'
+        }
+      ];
+    }
+    return [];
   }
 };
 

6dac31e005dd9a5acb6460a3097479e3c18671c8
Chamithu Ruberu <109074596+ChamithuRuberu@users.noreply.github.com>
2025-04-02 15:14:19 +0530

    ss

--- next.config.js
+++ next.config.js
@@ -18,7 +18,8 @@ const nextConfig = {
       return [
         {
           source: '/api/:path*',
-          destination: 'http://localhost:8080/api/:path*'
+          // destination: 'http://localhost:8080/api/:path*'
+          destination: '172.20.10.8:8080/api/:path*'
         }
       ];
     }
